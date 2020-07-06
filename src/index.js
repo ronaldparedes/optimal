044cb4c83c1442ac8a699d275e3043b7713756b1
Ronald <ronald@ww-lt09.cad.ww.com>
2020-07-06 14:55:05 -0400

    Completed responsive navbar and page navigation

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@ $(document).ready(function () {
     fade: true,
     dots: true,
   });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+  // On Click, scroll to top
+  $("#back-to-top").click(function () {
+    $("#back-to-top").tooltip("hide");
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+    return false;
+  });
+  $("#back-to-top").tooltip("show");
 });
